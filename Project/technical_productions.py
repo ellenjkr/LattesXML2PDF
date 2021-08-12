4797cc720edf581919d45124ab40d8273d40f5b9
Ellen Junker <ellenjunker2000@gmail.com>
2021-08-12 14:31:54 -0300

    Add softwares withou register

--- Project/technical_productions.py
+++ Project/technical_productions.py
@@ -10,6 +10,7 @@ class Technical_Productions():
 
 		advice_and_consultancy, other_technical_works = self.get_technical_works()
 		self.publications_dict["Assessoria e consultoria"] = advice_and_consultancy
+		self.publications_dict["Programas de computador sem registro"] = self.get_not_registered_softwares() # Softwares without register
 		self.publications_dict["Trabalhos técnicos"] = other_technical_works
 
 	def get_authors_string(self, xml_child):
@@ -73,3 +74,46 @@ class Technical_Productions():
 		advice_and_consultancy, other_technical_works = self.get_technical_works_strings(productions_df)
 
 		return advice_and_consultancy, other_technical_works
+
+	def get_softwares_strings(self, softwares_df):
+		softwares_strings = []
+
+		for pos, software in enumerate(softwares_df["title"]):
+			software_string = ""
+			software_string += f"{softwares_df['authors'][pos]}. {software}. {softwares_df['year'][pos]}."
+
+			softwares_strings.append(software_string)
+
+		return softwares_strings
+
+	def get_not_registered_softwares(self):
+		# Find the productions
+		xml_path = 'SOFTWARE'
+		softwares = self.xml_file.findall(f".//{xml_path}")
+
+		# Define softwares dictionary
+		softwares_dict = {"authors": [], "title": [], "year": []}
+
+		for software in softwares:
+			# Get the data
+			authors_string = self.get_authors_string(software)
+
+			basic_data = software.find(f".//DADOS-BASICOS-DO-SOFTWARE")
+			title = basic_data.attrib['TITULO-DO-SOFTWARE']
+			year = int(basic_data.attrib['ANO'])
+
+			details = software.find(f".//DETALHAMENTO-DO-SOFTWARE")
+
+			if details.find(".//REGISTRO-OU-PATENTE") is None: # Accept only the softwares without register
+				# Add data to the dictionary
+				softwares_dict["authors"].append(authors_string)
+				softwares_dict["title"].append(title)
+				softwares_dict["year"].append(year)
+
+		# Sort softwares by year
+		softwares_df = self.sort_by_key(softwares_dict, "year", ascending=False)
+		
+		# Generate strings for each software
+		softwares_strings = self.get_softwares_strings(softwares_df)
+
+		return softwares_strings
